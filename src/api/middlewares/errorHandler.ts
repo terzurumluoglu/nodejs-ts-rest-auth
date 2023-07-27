4b9e10898ba80642a09404d68a699192a43eafc2
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-07-27 22:55:49 +0300

    error handler for mongodb and server error

---
+++ src/api/middlewares/errorHandler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+
+const { ErrorResponse } = require("../utils");
+
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+
+    let error = { ...err };
+    error.message = err.message;
+
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    res.status(error.statusCode || 500).json({
+        success: false,
+        error: error.message || 'Server Error'
+    });
+};
