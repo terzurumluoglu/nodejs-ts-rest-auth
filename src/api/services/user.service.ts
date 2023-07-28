0240f9373b5d2b0cccd2c1b9573d7eb83f5f8344
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-07-28 23:08:22 +0300

    user collection path defined as a constant

--- src/api/services/user.service.ts
+++ src/api/services/user.service.ts
@@ -1,10 +1,12 @@
 import { getCollection } from "../../config/db";
 import { IUser } from "../models/IUser";
 
+const USER_COLLECTION: string = 'users';
+
 export class UserService {
 
     getUserByEmail = async (email: string): Promise<IUser> => {
-        const user = (await getCollection('users').findOne({ email })) as IUser;
+        const user = (await getCollection(USER_COLLECTION).findOne({ email })) as IUser;
         if (!user) {
             throw new Error(`There is no user this email: ${email}`);
         }
