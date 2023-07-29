2510dee4ae40af9d243b67127dbe985074e04a98
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-07-30 00:16:25 +0300

    save user function

--- src/api/services/user.service.ts
+++ src/api/services/user.service.ts
@@ -1,9 +1,13 @@
 import { getCollection } from "../../config/db";
-import { IUser } from "../models/IUser";
+import { IRegister } from "../models/ILogin";
+import { IToBeCreatedUser, IUser } from "../models/IUser";
+import { FacadeService } from "./facade.service";
 
 const USER_COLLECTION: string = 'users';
 
 export class UserService {
+
+    facade: FacadeService = FacadeService.get();
 
     getUserByEmail = async (email: string): Promise<IUser> => {
         const user = (await getCollection(USER_COLLECTION).findOne({ email })) as IUser;
@@ -11,5 +15,20 @@ export class UserService {
             throw new Error(`There is no user this email: ${email}`);
         }
         return user;
+    }
+
+    saveUser = async (body: IRegister) => {
+        const { name, email, password } = body;
+
+        const hashedPassword: string = await this.facade.hash(password);
+
+        const userToBeCreatedWithoutHashedPassword: IToBeCreatedUser = { name, email, createdAt: Date.now(), isActive: true, isDeleted: false, roles: ['user'] };
+
+        const userToBeCreated: IToBeCreatedUser = { ...userToBeCreatedWithoutHashedPassword, hashedPassword };
+
+        const { insertedId: _id } = await getCollection(USER_COLLECTION).insertOne(userToBeCreated);
+
+        return { _id, ...userToBeCreatedWithoutHashedPassword };
+
     }
 }
