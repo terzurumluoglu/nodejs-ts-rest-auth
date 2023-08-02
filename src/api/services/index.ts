2f6594c44988a187fcb3067efa05f82fde5670e4
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-08-02 22:49:19 +0300

    crypto service exported

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,5 +1,6 @@
 export * from './bcrypt.service';
 export * from './cookie.service';
+export * from './crypto.service';
 export * from './jwt.service';
 export * from './mail.service';
 export * from './user.service';
