1689bd455860c8eff3b2189e47a6e2834af14594
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-08-02 23:16:03 +0300

    routing forgot and reset password

--- src/api/routes/auth.route.ts
+++ src/api/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
-import { login, register } from '../controllers/auth.controller';
+import { forgotPassword, login, register, resetPassword } from '../controllers/auth.controller';
 import { asyncHandler } from '../middlewares/asyncHandler';
 
 const router: Router = express.Router();
 
 router.route('/login').post(asyncHandler(login));
 router.route('/register').post(asyncHandler(register));
+router.route('/forgotpassword').post(asyncHandler(forgotPassword));
+router.route('/resetpassword/:resetPasswordKey').post(asyncHandler(resetPassword));
 
 export { router as authRoute };
