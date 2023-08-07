5fddecb96f0a41c1ac8d880fb1cac810231a34de
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-08-07 22:20:40 +0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,41 @@
-import { NextFunction, Request, Response } from "express";
+import { Express, NextFunction, Request, Response } from "express";
 import swaggerUi from 'swagger-ui-express';
-import { app } from "./server";
+import { App } from "./server";
 
 import { Router } from "./api/routes/auth.route";
 import { errorHandler } from "./api/middlewares/errorHandler";
 import { OPTIONS } from './constants/swagger';
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.redirect('/docs');
-});
+export class Main {
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(OPTIONS));
+    static #instance: Main;
 
-app.use('/api', (req,res,next) => {
-    res.status(200).json({
-        message: 'Message from api path',
-    })
-});
+    static get(): Main {
+        if (!this.#instance) {
+            this.#instance = new Main();
+        }
+        return this.#instance;
+    }
 
-app.use('/auth', Router.get().routes);
-app.use(errorHandler);
+    constructor() {
+        const app: App = App.get();
+        const server: Express = app.server;
+
+        server.get('/', (req: Request, res: Response, next: NextFunction) => {
+            res.redirect('/docs');
+        });
+
+        server.use('/docs', swaggerUi.serve, swaggerUi.setup(OPTIONS));
+
+        server.use('/api', (req, res, next) => {
+            res.status(200).json({
+                message: 'Message from api path',
+            })
+        });
+
+        server.use('/auth', Router.get().routes);
+        server.use(errorHandler);
+    }
+}
+
+Main.get();
--- src/server/index.ts
+++ src/server/index.ts
@@ -5,27 +5,45 @@ import cookieParser from 'cookie-parser';
 import { Database } from '../config/database';
 import { BASE_PATH, ENVIRONMENTS } from "../constants";
 
-export const app: Express = express();
+export class App {
+    #server: Express = express();
 
-const PORT = process.env.PORT || 8080;
+    #PORT = process.env.PORT || 8080;
 
-let environmentPath = `${BASE_PATH}environment.env`;
+    #environmentPath = `${BASE_PATH}environment.env`;
 
-if (process.env.ENVIRONMENT === ENVIRONMENTS.PRODUCTION) {
-    environmentPath = `${BASE_PATH}environment.prod.env`;
+    static #instance: App;
+
+    static get(): App {
+        if (!this.#instance) {
+            this.#instance = new App();
+        }
+        return this.#instance;
+    }
+
+    constructor() {
+
+        if (process.env.ENVIRONMENT === ENVIRONMENTS.PRODUCTION) {
+            this.#environmentPath = `${BASE_PATH}environment.prod.env`;
+        }
+
+        dotenv.config({ path: this.#environmentPath });
+
+        if (process.env.ENVIRONMENT === ENVIRONMENTS.DEVELOPMENT) {
+            this.#server.use(cors({ origin: '*', methods: '*' }));
+        }
+
+        this.#server.use(express.json());
+        this.#server.use(cookieParser());
+
+        Database.get();
+
+        this.#server.listen(this.#PORT, () => {
+            console.log(`The server is running on: ${this.#PORT}`);
+        });
+    }
+
+    get server() {
+        return this.#server;
+    }
 }
-
-dotenv.config({ path: environmentPath });
-
-if (process.env.ENVIRONMENT === ENVIRONMENTS.DEVELOPMENT) {
-    app.use(cors({ origin: '*', methods: '*' }));
-}
-
-app.use(express.json());
-app.use(cookieParser());
-
-Database.get();
-
-app.listen(PORT, () => {
-    console.log(`The server is running on: ${PORT}`);
-});
