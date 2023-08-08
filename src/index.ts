a041a33c667dc48132465a541c90c84ca714c71f
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-08-08 21:29:48 +0300

    no necessary codes and name changed

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,26 @@
 import { Express, NextFunction, Request, Response } from "express";
 import swaggerUi from 'swagger-ui-express';
-import { App } from "./server";
+import { App } from "./server/app";
 
 import { Router } from "./api/routes/auth.route";
 import { errorHandler } from "./api/middlewares/errorHandler";
 import { OPTIONS } from './constants/swagger';
 
-export class Main {
+const app: App = App.get();
 
-    static #instance: Main;
+const server: Express = app.server;
 
-    static get(): Main {
-        if (!this.#instance) {
-            this.#instance = new Main();
-        }
-        return this.#instance;
-    }
+server.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.redirect('/docs');
+});
 
-    constructor() {
-        const app: App = App.get();
-        const server: Express = app.server;
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(OPTIONS));
 
-        server.get('/', (req: Request, res: Response, next: NextFunction) => {
-            res.redirect('/docs');
-        });
+server.use('/api', (req, res, next) => {
+    res.status(200).json({
+        message: 'Message from api path',
+    })
+});
 
-        server.use('/docs', swaggerUi.serve, swaggerUi.setup(OPTIONS));
-
-        server.use('/api', (req, res, next) => {
-            res.status(200).json({
-                message: 'Message from api path',
-            })
-        });
-
-        server.use('/auth', Router.get().routes);
-        server.use(errorHandler);
-    }
-}
-
-Main.get();
+server.use('/auth', Router.get().routes);
+server.use(errorHandler);
